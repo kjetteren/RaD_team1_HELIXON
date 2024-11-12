8d8f882ffd44d4b42dbc0543a64203d017308a87
Ruslan Galiullin <160370317+kjetteren@users.noreply.github.com>
2024-11-12 13:43:45 +0100

    First version of the AP Wi-Fi UDP test

--- Arduino/apwifi/apwifi.ino
+++ Arduino/apwifi/apwifi.ino
@@ -1,19 +1,13 @@
 /*
-  WiFi Web Server LED Blink
+  WiFi AP UDP packet loss test
 
-  A simple web server that lets you blink an LED via the web.
-  This sketch will create a new access point (with no password).
-  It will then launch a new server and print out the IP address
-  to the Serial Monitor. From there, you can open that address in a web browser
-  to turn on and off the LED on pin 13.
+  A simple UDP packet loss test. This sketch will create a new access point
+  (with specified password). It will then launch a new server and print out 
+  the IP address to the Serial Monitor. A Python script needs to be ran at
+  the same time to send the UDP packets.
 
-  If the IP address of your board is yourAddress:
-    http://yourAddress/H turns the LED on
-    http://yourAddress/L turns it off
-
-  created 25 Nov 2012
-  by Tom Igoe
-  adapted to WiFi AP by Adafruit
+  created 12 Nov 2024
+  by Ruslan Galiullin
  */
 
 #include <SPI.h>
@@ -23,9 +17,11 @@
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;                // your network key index number (needed only for WEP)
+WiFiUDP udp;                      // UDP object
+unsigned int localPort = 12345;   // Port to listen on
 
-int status = WL_IDLE_STATUS;
-WiFiServer server(80);
+int receivedPackets = 0;          // Counts received packets
+int expectedPackets = 100;        // Expected number of packets to receive
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -34,7 +30,7 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  Serial.println("Access Point Web Server");
+  Serial.println("Starting UDP Packet Loss Test");
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
@@ -67,8 +63,9 @@ void setup() {
   // wait 10 seconds for connection:
   delay(10000);
 
-  // start the web server on port 80
-  server.begin();
+  // start listening to the specified port
+  udp.begin(localPort);
+  Serial.println("UDP server started, waiting for packets...");
 
   // you're connected now, so print out the status
   printWiFiStatus();
@@ -76,70 +73,28 @@ void setup() {
 
 
 void loop() {
-  // compare the previous status to the current status
-  if (status != WiFi.status()) {
-    // it has changed update the variable
-    status = WiFi.status();
+  int packetSize = udp.parsePacket(); // Check for incoming packets
 
-    if (status == WL_AP_CONNECTED) {
-      // a device has connected to the AP
-      Serial.println("Device connected to AP");
-    } else {
-      // a device has disconnected from the AP, and we are back in listening mode
-      Serial.println("Device disconnected from AP");
+  if (packetSize) {
+    // Packet received
+    receivedPackets++;
+    Serial.print("Received packet #");
+    Serial.println(receivedPackets);
+
+    // Clear buffer and receive data (optional, for packet integrity check)
+    char packetBuffer[255];
+    udp.read(packetBuffer, 255); // Read incoming packet
+
+    // Calculate and display packet loss percentage if we reach the expected count
+    if (receivedPackets >= expectedPackets) {
+      float packetLossPercent = 100.0 * (1.0 - (float(receivedPackets) / float(expectedPackets)));
+      Serial.print("Packet loss: ");
+      Serial.print(packetLossPercent);
+      Serial.println("%");
+
+      // Reset count for next round of tests
+      receivedPackets = 0;
     }
-  }
-  
-  WiFiClient client = server.available();   // listen for incoming clients
-
-  if (client) {                             // if you get a client,
-    Serial.println("new client");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      delayMicroseconds(10);                // This is required for the Arduino Nano RP2040 Connect - otherwise it will loop so fast that SPI will never be served.
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out to the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> send the /H message<br>");
-            client.print("Click <a href=\"/L\">here</a> send the /L message<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          }
-          else {      // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        }
-        else if (c != '\r') {    // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          Serial.println("GET /H was accessed");       // GET /H
-        }
-        if (currentLine.endsWith("GET /L")) {
-          Serial.println("GET /H was accessed");       // GET /L
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
   }
 }
 
