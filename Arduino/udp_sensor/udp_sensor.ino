378bfdfedb50805d89a84b656dee09115b07b55b
Ruslan Galiullin <160370317+kjetteren@users.noreply.github.com>
2024-11-19 12:12:38 +0100

    Added the sensors setup code for Wi-Fi UDP sensor transfer

--- Arduino/udp_sensor/udp_sensor.ino
+++ Arduino/udp_sensor/udp_sensor.ino
@@ -10,19 +10,39 @@
   by Ruslan Galiullin
  */
 
+#include <Wire.h>
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <Adafruit_BMP3XX.h>
 #include "arduino_secrets.h" 
+
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;        // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;                 // your network key index number (needed only for WEP)
 WiFiUDP udp;                      // UDP object
 unsigned int localPort = 12345;   // Port to listen on
-
 int status = WL_IDLE_STATUS;
 
+// sensor setup
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28); // BNO055 has an I2C address of 0x28 by default
+Adafruit_BMP3XX bmp;
+float a_x,a_y,a_z,m_x,m_y,m_z,omega_x,omega_y,omega_z,g_x,g_y,g_z,pressure,temperature;
+imu::Vector<3> magneto;
+imu::Vector<3> gravity;
+imu::Vector<3> gyro;
+imu::Vector<3> acceleration;
+
 void setup() {
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_2X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_16X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_25_HZ);
+
+  bno.setMode(OPERATION_MODE_NDOF);
+
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     // don't continue
